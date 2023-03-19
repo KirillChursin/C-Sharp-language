00591f9244098d0c7c9f84eb8b10ad29de2de3ba
Kirill Chursin <kirill.chursin@mail.ru>
2023-03-19 13:33:39 +0300

    task 2

--- Work 18.03/Home Work/Task 2/Program.cs
+++ Work 18.03/Home Work/Task 2/Program.cs
@@ -7,7 +7,44 @@
 // 9012 -> 12
 
 
-int arg1(string x)
+int writeRead (string message)
 {
-
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
 }
+
+
+int sumAllDigit (int arg)
+{
+    int result = 0;
+    while (arg>0)
+    {
+        result += arg % 10;
+        arg = arg / 10;
+    }
+    return result;
+}
+
+int numb = writeRead($"Enter number");
+Console.WriteLine(sumAllDigit(numb));
+
+
+// Console.WriteLine($"Enter number");
+// int numb = Convert.ToInt32(Console.ReadLine()); превращаем это в метод выводящий сообщение введите число и возвращающий вводимое пользоваетелем значение
+int myNumb (string message)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+int numb = myNumb($"Enter number");
+
+int result = 0;
+while (numb > 0)
+{
+    result = result + numb % 10;
+    numb = numb / 10;
+}
+Console.WriteLine(result);
+
